c53c7b806782b0c6994bc3a09002adc9e0bff092
ldhitesh <ldhitesh.02@gmail.com>
2024-11-29 17:45:27 -0600

    regrations

---
+++ src/app/Components/registrations/registrations.component.ts
@@ -0,0 +1,92 @@
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-registrations',
+  templateUrl: './registrations.component.html',
+  styleUrls: ['./registrations.component.css']
+})
+export class RegistrationsComponent {
+
+  pendingapprovals: any=[];
+  public pageSize = 8;
+  public currentPage = 1;
+  public totalPages:any;
+  public nomatchingdata:boolean=false;
+  public UserName:any;
+  public UserRole:any
+
+  constructor(private http:HttpClient,private router:Router,
+                      private activatedroute:ActivatedRoute
+  ){}
+
+
+  ngOnInit(): void {
+    this.fetchPendingApprovals();
+    this.UserName = this.activatedroute.snapshot.paramMap.get('UserName');
+    this.UserRole=localStorage.getItem(this.UserName);  
+  }
+
+  fetchPendingApprovals(): void {
+    this.http.get('http://localhost:5018/api/register/getpendingregistrations').subscribe({
+      next: (data) => {
+        this.pendingapprovals=data;    
+        this.totalPages=Math.ceil(this.pendingapprovals.length/this.pageSize);  
+
+      },
+      error: (err) => {
+        console.error('Error fetching companies:', err);
+      }
+    });
+  }
+  get paginatedData() {
+    const startIndex = (this.currentPage-1) * this.pageSize;
+    const endIndex = startIndex + this.pageSize;
+    return this.pendingapprovals.slice(startIndex, endIndex);
+  }
+
+  approveUser(user: any): void {    
+    this.http.post('http://localhost:5018/api/register',user).subscribe({
+      next:(response:any)=>{
+        alert(response.message);
+        this.fetchPendingApprovals();
+      },
+      error:(err:any)=>{       
+        const errorMessage = err.error?.message || 'An unexpected error occurred.';
+
+        alert(errorMessage);   
+      }
+    })
+  }
+
+  rejectUser(user: any): void {
+    
+    this.http.delete(`http://localhost:5018/api/register/rejectregistration/${user.username}`).subscribe({
+      next:(response:any)=>{
+        alert(response.message);
+        this.fetchPendingApprovals();
+      },
+      error:(err:any)=>{       
+        const errorMessage = err.error?.message || 'An unexpected error occurred.';
+
+        alert(errorMessage);   
+      }
+    })
+  }
+
+
+  PrevClicked(){
+    if(this.currentPage>1 ){
+      this.currentPage-=1;
+      this.paginatedData
+    }
+  }
+
+  NextClicked(){
+    if(this.currentPage<this.totalPages){
+      this.currentPage+=1;
+      this.paginatedData
+    }
+  }
+}
